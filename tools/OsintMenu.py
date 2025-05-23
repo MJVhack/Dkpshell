88f192730d4914d65ffb4d482ed8e1db9e243c54
DKPDEV <113791532+MJVhack@users.noreply.github.com>
2025-05-23 23:57:40 +0200

    Update OsintMenu.py

--- tools/OsintMenu.py
+++ tools/OsintMenu.py
@@ -1,37 +1,39 @@
+from .Colors import * 
+
 def OsintMenu():
 
 
     def menu():
-        print(f"{MAGENTA}\n=== QG OSINT - Interface Terminal BY SA |Timeline ===")
+        print(f"{Colors.MAGENTA}\n=== QG OSINT - Interface Terminal BY SA |Timeline ===")
         print("")
-        print(f"{YELLOW}Lancez en sudo pour installer les modules{RESET}")
+        print(f"{Colors.YELLOW}Lancez en sudo pour installer les modules{Colors.RESET}")
         print("")
-        print(f"{BLUE}[1] Sherlock (Username Search)")
+        print(f"{Colors.BLUE}[1] Sherlock (Username Search)")
         print(f"[2] Linkook (Social Link OSINT)")
         print(f"[3] Holehe (Email Verification)")
         print(f"[4] Nmap (Scan réseau)")
         print(f"[5] SQLMap (Injection SQL)")
-        print(f"[0] Quitter{RESET}")
-        return input(f"{ORANGE}Choisis une option > {RESET}")
+        print(f"[0] Quitter{Colors.RESET}")
+        return input(f"{Colors.ORANGE}Choisis une option > {Colors.RESET}")
 
     def run_sherlock():
-        username = input(f"{CYAN}Entrez le pseudo à chercher : ")
+        username = input(f"{Colors.CYAN}Entrez le pseudo à chercher : ")
         subprocess.run(f'cd ~/sherlock/sherlock_project && python3 sherlock.py {username}', shell=True)
 
     def run_linkook():
-        username = input(f"{CYAN}Entrez le pseudo à chercher (Linkook) : ")
+        username = input(f"{Colors.CYAN}Entrez le pseudo à chercher (Linkook) : ")
         subprocess.run(f'~/.local/share/pipx/venvs/linkook/bin/linkook {username}', shell=True)
 
     def run_holehe():
-        email = input(f"{CYAN}Entrez l'adresse email : ")
+        email = input(f"{Colors.CYAN}Entrez l'adresse email : ")
         subprocess.run(f'holehe {email}', shell=True)
 
     def run_nmap():
-        target = input(f"{CYAN}Entrez l'adresse IP ou domaine : ")
+        target = input(f"{Colors.CYAN}Entrez l'adresse IP ou domaine : ")
         subprocess.run(f'nmap -sV -T4 -Pn --script vuln {target}', shell=True)
 
     def run_sqlmap():
-        url = input(f"{CYAN}Entrez l'URL vulnérable (avec paramètre) : ")
+        url = input(f"{Colors.CYAN}Entrez l'URL vulnérable (avec paramètre) : ")
         subprocess.run(f'sqlmap -u "{url}" --batch --level=3 --risk=2', shell=True)
 
 # Boucle principale
