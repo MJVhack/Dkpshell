307558db9f5e8ee7a9e53407c25c0baa5b3aa95d
DKPDEV <113791532+MJVhack@users.noreply.github.com>
2025-05-23 23:38:58 +0200

    Update dkpshell.py [MAJOR] - test

--- dkpshell.py
+++ dkpshell.py
@@ -23,10 +23,10 @@ import asyncio
 from tools import *
 
 
-__version__ = "4.4"
+__version__ = "5.0"
 __stable__ = False
 
-prompt_color = BLUE
+prompt_color = Colors.BLUE
 
 def check_stability():
     update_url = "https://raw.githubusercontent.com/MJVhack/MJVhack/refs/heads/main/dkpshell.py"
@@ -39,13 +39,13 @@ def check_stability():
 
         if stable_match:
             is_stable = stable_match.group(1) == "True"
-            status = f"{GREEN}stable{RESET}" if is_stable else f"{YELLOW}instable{RESET}"
-            print(f"{CYAN}[INFO] La version distante est : {status}")
+            status = f"{Colors.GREEN}stable{Colors.RESET}" if is_stable else f"{Colors.YELLOW}instable{Colors.RESET}"
+            print(f"{Colors.CYAN}[INFO] La version distante est : {status}")
         else:
-            print(f"{RED}[Erreur] Clé '__stable__' non trouvée dans le script distant.{RESET}")
+            print(f"{Colors.RED}[Erreur] Clé '__stable__' non trouvée dans le script distant.{Colors.RESET}")
 
     except Exception as e:
-        print(f"{RED}[Erreur] Impossible de vérifier la stabilité distante : {e}{RESET}")
+        print(f"{Colors.RED}[Erreur] Impossible de vérifier la stabilité distante : {e}{Colors.RESET}")
 
 
 def check_update():
@@ -59,11 +59,11 @@ def check_update():
         if match:
             remote_version = match.group(1)
             if remote_version > __version__:
-                print(f"{MAGENTA}[DKP Shell] : Une mise à jour est disponible ({__version__} → {remote_version}){RESET}")
-                print(f"{CYAN}➜ Lance la commande `dkpupdate` pour mettre à jour{RESET}")
+                print(f"{Colors.MAGENTA}[DKP Shell] : Une mise à jour est disponible ({__version__} → {remote_version}){Colors.RESET}")
+                print(f"{Colors.CYAN}➜ Lance la commande `dkpupdate` pour mettre à jour{Colors.RESET}")
                 check_stability()
     except Exception as e:
-        print(f"{RED}[DKP Shell] : Échec de vérification de mise à jour : {e}\033[0m")
+        print(f"{Colors.RED}[DKP Shell] : Échec de vérification de mise à jour : {e}\033[0m")
         
 
 histfile = os.path.expanduser("~/.dkpshell_history")
@@ -118,24 +118,6 @@ def completer(text, state):
 
 readline.set_completer(completer)
 
-def color_config():
-    print(f'''{RED}RED 
-{GREEN}GREEN
-{YELLOW}YELLOW 
-{CYAN}CYAN 
-{MAGENTA}MAGENTA 
-{RESET}RESET 
-{BOLD}BOLD 
-{BLUE}BLUE 
-{ORANGE}ORANGE ''')
-    print("mettait EXACTEMENT le nom des variables")
-    A_color = input("")
-    B_color = input("")
-    C_color = input("")
-    D_color = input("")
-    E_color = input("")
-    shell_input1 = input(f"{A_color}┌──({B_color}{custom_prompt}{C_color}{root_state}{D_color}{user})-[{E_color}~]\n└─[ {RESET}")
-
 # Fonction pour détecter si root
 def is_root():
     return os.geteuid() == 0
@@ -143,26 +125,26 @@ def is_root():
 
 def install_all():
     os.system("pip install -U Pyreadline3")
-    print(f"{GREEN}Pyreadline3 succesful installed")
+    print(f"{Colors.GREEN}Pyreadline3 succesful installed")
     os.system("pip install -U discord.py")
-    print(f"{GREEN}Discord.py succesful installed")
+    print(f"{Colors.GREEN}Discord.py succesful installed")
     os.system("sudo apt install sherlock")
-    print(f"{GREEN}Sherlock succeful installed{RESET}")
+    print(f"{Colors.GREEN}Sherlock succeful installed{Colors.RESET}")
     os.system("pipx install linkook")
-    print(f"{GREEN}Linkook succeful installed{RESET}")
+    print(f"{Colors.GREEN}Linkook succeful installed{Colors.RESET}")
     os.system("git clone https://github.com/megadose/holehe.git && cd holehe/ && python3 setup.py install")
-    print(f"{GREEN}Holehe succeful installed{RESET}")
+    print(f"{Colors.GREEN}Holehe succeful installed{Colors.RESET}")
     os.system("sudo apt install nmap")
-    print(f"{GREEN}Nmap succesful installed{RESET}")
+    print(f"{Colors.GREEN}Nmap succesful installed{Colors.RESET}")
     os.system("sudo apt install sqlmap")
-    print(f"{GREEN}Sqlmap succesful installed{RESET}")
+    print(f"{Colors.GREEN}Sqlmap succesful installed{Colors.RESET}")
     os.system("pip install discord.py")
-    print(f"{GREEN}Discord.py succcesful installed{RESET}")
+    print(f"{Colors.GREEN}Discord.py succcesful installed{Colors.RESET}")
 
-updlist = f"""{YELLOW}NEW ADD{BLUE}
+updlist = f"""{Colors.YELLOW}NEW ADD{Colors.BLUE}
 --------------3.0--------------------
 [+] add '{cmd_for_config} -installall', '{cmd_for_config.replace("config", "tool")} -e RaidDiscordBD', '{cmd_for_config} -updlist'
-    [*]{cmd_for_config} -installall: Avant pour installer les modules, il fallait lancer Osint Menu, plus maintenant. Desormais meme les modules pour {MAGENTA}[dkpshell.py]{BLUE} sont installer et update via {cmd_for_config} -installall
+    [*]{cmd_for_config} -installall: Avant pour installer les modules, il fallait lancer Osint Menu, plus maintenant. Desormais meme les modules pour {Colors.MAGENTA}[dkpshell.py]{Colors.BLUE} sont installer et update via {cmd_for_config} -installall
     [*]{cmd_for_config.replace("config", "tool")} -e RaidDiscordBD: Permet de lancer le nnouveau module 'raid_discord' V1 avec IMPERATIVEMENT un token de bot DISCORD
     [*]{cmd_for_config} -updlist: réaffiche ce que vous lisez maintenant
 [+]add 'The Update list'
@@ -188,7 +170,7 @@ updlist = f"""{YELLOW}NEW ADD{BLUE}
     [*]'{cmd_for_config} -version' affiche votre version actuelle
     """
 # Affichage ASCII Art
-ascii_art = fr"""{CYAN}
+ascii_art = fr"""{Colors.CYAN}
 <!-- .------------------------------------------------------------------------------------------------. -->
 <!-- |                                                                                                | -->
 <!-- |                                                                                                | -->
@@ -211,26 +193,26 @@ ascii_art = fr"""{CYAN}
                                            version : {__version__}
 
 {updlist}
-{RESET}
+{Colors.RESET}
 """
 os.system("clear")
 print(ascii_art)
 check_update()
 print("")
-print(f"{YELLOW}💡 Tips: Lancer en sudo pour accéder à toutes les fonctionnalités.\n{RESET}")
+print(f"{Colors.YELLOW}💡 Tips: Lancer en sudo pour accéder à toutes les fonctionnalités.\n{Colors.RESET}")
 
 # État Root
 if is_root():
-    print(f"{GREEN}[État] : ✅ Rooted{RESET}")
+    print(f"{Colors.GREEN}[État] : ✅ Rooted{Colors.RESET}")
 else:
-    print(f"{RED}[État] : ❌ No Rooted{RESET}")
+    print(f"{Colors.RED}[État] : ❌ No Rooted{Colors.RESET}")
 
 # Texte de prompt
 custom_prompt = input("\n[?] Texte pour le prompt du shell (ex: DKP{㉿ ) (ajoute un espace à la fin) : ")
 print("")
 # Ajouter au PATH
-print(f"{YELLOW}💡 Tips: Redire 'yes' updatera la copie du script se trouvant dans le /bin")
-add_to_path = input(f"{YELLOW}\n[?] Voulez-vous ajouter ce script/update à /usr/local/bin (nécessite sudo) ? (yes/no) : ").lower()
+print(f"{Colors.YELLOW}💡 Tips: Redire 'yes' updatera la copie du script se trouvant dans le /bin")
+add_to_path = input(f"{Colors.YELLOW}\n[?] Voulez-vous ajouter ce script/update à /usr/local/bin (nécessite sudo) ? (yes/no) : ").lower()
 
 if add_to_path == "yes":
     script_path = os.path.realpath(__file__)
@@ -238,18 +220,18 @@ if add_to_path == "yes":
     try:
         shutil.copy(script_path, target_path)
         os.chmod(target_path, 0o755)
-        print(f"{GREEN}[+] Script copié dans {target_path} ✅{RESET}")
+        print(f"{Colors.GREEN}[+] Script copié dans {target_path} ✅{Colors.RESET}")
     except PermissionError:
-        print(f"{ORANGE}[!] Permission refusée. Relancez en sudo pour l'ajouter au PATH.{RESET}")
+        print(f"{Colors.ORANGE}[!] Permission refusée. Relancez en sudo pour l'ajouter au PATH.{Colors.RESET}")
     except Exception as e:
-        print(f"{RED}[!] Erreur lors de la copie : {e}{RESET}")
+        print(f"{Colors.RED}[!] Erreur lors de la copie : {e}{Colors.RESET}")
 
 
 # Déclenchement du shell
 if is_root():
-        start = input(f"{RED}\nTapez 'start' pour lancer le DKP Shell : {RESET}").strip().lower()
+        start = input(f"{Colors.RED}\nTapez 'start' pour lancer le DKP Shell : {Colors.RESET}").strip().lower()
 else:
-        start = input(f"{MAGENTA}\nTapez 'start' pour lancer le DKP Shell : {RESET}").strip().lower()
+        start = input(f"{Colors.MAGENTA}\nTapez 'start' pour lancer le DKP Shell : {Colors.RESET}").strip().lower()
 
 
 
@@ -261,20 +243,20 @@ def shell():
     try:
         while True:
             print("")
-            shell_input = input(f"{prompt_color}┌──({custom_prompt}{root_state}{user})-[~]\n└─[ {RESET}")
+            shell_input = input(f"{prompt_color}┌──({custom_prompt}{root_state}{user})-[~]\n└─[ {Colors.RESET}")
             print("")
             if shell_input in [f"{cmd_for_config}", f"{cmd_for_config} --help"]:
-                print(f"{MAGENTA}DKPSHELL help")
+                print(f"{Colors.MAGENTA}DKPSHELL help")
                 print("")
-                print(f"{MAGENTA}{cmd_for_config} -colorlist {RESET}")
-                print(f"{MAGENTA}{cmd_for_config.replace('config', 'update')}{RESET}")
-                print(f"{MAGENTA}{cmd_for_config.replace('config', 'tool')}{RESET}")
-                print(f"{MAGENTA}{cmd_for_config} -restartshell")
-                print(f"{MAGENTA}{cmd_for_config} -exit")
-                print(f"{MAGENTA}{cmd_for_config} -updlist")
+                print(f"{Colors.MAGENTA}{cmd_for_config} -colorlist {Colors.RESET}")
+                print(f"{Colors.MAGENTA}{cmd_for_config.replace('config', 'update')}{Colors.RESET}")
+                print(f"{Colors.MAGENTA}{cmd_for_config.replace('config', 'tool')}{Colors.RESET}")
+                print(f"{Colors.MAGENTA}{cmd_for_config} -restartshell")
+                print(f"{Colors.MAGENTA}{cmd_for_config} -exit")
+                print(f"{Colors.MAGENTA}{cmd_for_config} -updlist")
                 continue
             elif shell_input in [f"{cmd_for_config} -colorlist"]:
-                 print(f'''{MAGENTA}DKPSHELL color
+                 print(f'''{Colors.MAGENTA}DKPSHELL color
                  
                  RED = {cmd_for_config} -color red
                  GREEN = {cmd_for_config} -color green
@@ -287,31 +269,31 @@ def shell():
                  ORANGE = {cmd_for_config} -color orange''')
                  continue
             elif shell_input in [f"{cmd_for_config} -color red"]:
-                 prompt_color = RED
+                 prompt_color = Colors.RED
                  continue
             elif shell_input in [f"{cmd_for_config} -color green"]:
-                 prompt_color = GREEN
+                 prompt_color = Colors.GREEN
                  continue
             elif shell_input in [f"{cmd_for_config} -color yellow"]:
-                 prompt_color = YELLOW
+                 prompt_color = Colors.YELLOW
                  continue
             elif shell_input in [f"{cmd_for_config} -color cyan"]:
-                 prompt_color = CYAN
+                 prompt_color = Colors.CYAN
                  continue
             elif shell_input in [f"{cmd_for_config} -color magenta"]:
-                 prompt_color = MAGENTA
+                 prompt_color = Colors.MAGENTA
                  continue
             elif shell_input in [f"{cmd_for_config} -color white"]:
-                 prompt_color = WHITE
+                 prompt_color = Colors.WHITE
                  continue
             elif shell_input in [f"{cmd_for_config} -color bold"]:
-                 prompt_color = BOLD
+                 prompt_color = Colors.BOLD
                  continue
             elif shell_input in [f"{cmd_for_config} -color blue"]:
-                 prompt_color = BLUE
+                 prompt_color = Colors.BLUE
                  continue
             elif shell_input in [f"{cmd_for_config} -color orange"]:
-                 prompt_color = ORANGE
+                 prompt_color = Colors.ORANGE
                  continue
             elif shell_input in [f"{cmd_for_config.replace("config", "tool")} -e RaidDiscordBD"]:
                 raid_discord()
@@ -321,18 +303,18 @@ def shell():
                 
                 
             elif shell_input in [f"{cmd_for_config} -restartshell"]:
-                print(f"{YELLOW}[DKP Shell] : Redémarrage du shell...{RESET}")
+                print(f"{Colors.YELLOW}[DKP Shell] : Redémarrage du shell...{Colors.RESET}")
                 python_exe = sys.executable  # Chemin vers l'interpréteur Python
                 script_path = os.path.realpath(__file__)  # Chemin vers le script courant
                 os.execv(python_exe, [python_exe, script_path])  # Relance le script
 
             
             elif shell_input in [f"{cmd_for_config} -exit"]:
-                print(f"{GREEN}[DKP Shell] : Fermeture{RESET}")
+                print(f"{Colors.GREEN}[DKP Shell] : Fermeture{Colors.RESET}")
                 sys.exit(0)
 
             elif shell_input in [f"{cmd_for_config.replace('config', 'tool')} --help",f"{cmd_for_config.replace('config', 'tool')}" ]:
-                print(f"{MAGENTA}LIST")
+                print(f"{Colors.MAGENTA}LIST")
                 print("")
                 print("[OSINT MENU]: dkptool -e OsintMenu")
                 print("[RAID DISCORD BY BOT DISCORD]: dkptool -e RaidDiscordBD")
@@ -342,7 +324,7 @@ def shell():
                 OsintMenu()
 
             elif shell_input == f"{cmd_for_config} -color config":
-                print(f"{ORANGE}Cette option a été momontanément désactiver.")
+                print(f"{Colors.ORANGE}Cette option a été momontanément désactiver.")
 
             elif shell_input == f"{cmd_for_config} -updlist":
                 print(updlist)
@@ -354,7 +336,7 @@ def shell():
                 check_stability()
                 
             elif shell_input == f"{cmd_for_config.replace('config', 'update')}":
-                print(f"{CYAN}[DKP Shell] : Mise à jour en cours...{RESET}")
+                print(f"{Colors.CYAN}[DKP Shell] : Mise à jour en cours...{Colors.RESET}")
                 try:
                     # Variables
                     update_url = "https://raw.githubusercontent.com/MJVhack/MJVhack/refs/heads/main/dkpshell.py"  # 🔁 Modifie ici
@@ -363,40 +345,40 @@ def shell():
 
                     # Télécharger nouvelle version
                     urllib.request.urlretrieve(update_url, local_script)
-                    print(f"{GREEN}[✓] Script local mis à jour : {local_script}{RESET}")
+                    print(f"{Colors.GREEN}[✓] Script local mis à jour : {local_script}{Colors.RESET}")
 
                     # Copier vers /usr/local/bin (accès global)
                     if os.path.exists(bin_path) or os.access(os.path.dirname(bin_path), os.W_OK):
                         shutil.copy(local_script, bin_path)
                         os.chmod(bin_path, 0o755)
-                        print(f"{GREEN}[✓] Script copié dans {bin_path}{RESET}")
+                        print(f"{Colors.GREEN}[✓] Script copié dans {bin_path}{Colors.RESET}")
                     else:
-                        print(f"{YELLOW}[!] Pas de permission pour écrire dans {bin_path}. Skipped.{RESET}")
+                        print(f"{Colors.YELLOW}[!] Pas de permission pour écrire dans {bin_path}. Skipped.{Colors.RESET}")
 
                     # Redémarrer le shell automatiquement
-                    print(f"{CYAN}[DKP Shell] : Redémarrage du shell...{RESET}")
+                    print(f"{Colors.CYAN}[DKP Shell] : Redémarrage du shell...{Colors.RESET}")
                     python_exe = sys.executable
                     os.execv(python_exe, [python_exe, local_script])
                     
                 except Exception as e:
-                    print(f"{RED}[Erreur] : La mise à jour a échoué : {e}{RESET}")
+                    print(f"{Colors.RED}[Erreur] : La mise à jour a échoué : {e}{Colors.RESET}")
                 continue
 
                 
 
             else:
-                print(f"{RED}Commande non reconnu en tant que commande {MAGENTA}[DKP]")
-                print(f"{YELLOW}")
+                print(f"{Colors.RED}Commande non reconnu en tant que commande {Colors.MAGENTA}[DKP]")
+                print(f"{Colors.YELLOW}")
                 os.system(shell_input)
             continue
 
 
 
     except KeyboardInterrupt:
-        print(f"{GREEN}\n[DKP Shell]: Fermeture.{RESET}")
+        print(f"{Colors.GREEN}\n[DKP Shell]: Fermeture.{Colors.RESET}")
         exit()
 
 if start == "start":
     shell()
 else:
-    print(f"{CYAN}[Script]: Fin du script.{RESET}")
+    print(f"{Colors.CYAN}[Script]: Fin du script.{Colors.RESET}")
