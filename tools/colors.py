052b858964721289d5e7a24796395f6279ad0eb9
DKPDEV <113791532+MJVhack@users.noreply.github.com>
2025-05-23 22:05:36 +0200

    Create colors.py

---
+++ tools/colors.py
@@ -0,0 +1,10 @@
+class Colors:
+  RED = "\033[91m"
+  GREEN = "\033[92m"
+  YELLOW = "\033[93m"
+  CYAN = "\033[96m"
+  MAGENTA = "\033[95m"
+  RESET = "\033[0m"
+  BOLD = "\033[1m"
+  BLUE = "\033[94m"
+  ORANGE = "\033[38;5;208m"
